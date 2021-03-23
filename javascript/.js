42e2177330be29412f43b74ab05fc4445b99abf4
“William <umachattahendo@mail.com>
2021-03-23 10:21:41 -0400

    add css

---
+++ javascript/.js
@@ -0,0 +1,22 @@
+
+let selOrange= ['round', 'orange'];
+let selLime= ['green', 'oblong'];
+let selLemon= ['yellow','oblong'];
+let selGrapefruit=['round', 'yellow'];
+
+var monoPrompt = window.prompt("Is the fruit round or oblong?");
+var duoPrompt = window.prompt("Is the fruit orange?"); /*no=0, yes=1 */
+var triPrompt = window.prompt("Is the fruit yellow?");/* */
+var tetraPrompt = window.prompt("Is the fruit green?");
+
+if monoPrompt= (/*selOrange 0*/){
+    //
+}
+    else if {
+        //
+    }
+    else if {
+
+    else if {
+    }
+}
