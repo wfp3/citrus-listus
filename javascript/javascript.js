7151bf1c888b5c1e42cba2ec55a3dfff1f4aa0b9
“William <umachattahendo@mail.com>
2021-03-24 15:48:13 -0400

    repairs file links

---
+++ javascript/javascript.js
@@ -0,0 +1,65 @@
+function validateForm() {
+    var x = /Users/williampolk/Documents/citrus-listus/index.html["freeTreeForm"]["firstname"].value;
+    if (x == "") {
+      alert("First name must be filled out");
+      return false;
+    }
+  } 
+
+  function validateForm() {
+    var x = document.forms["freeTreeForm"]["lastname"].value;
+    if (x == "") {
+      alert("Last name must be filled out; we need to mine your data, duh.");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["myFormfreeTreeForm"]["fname"].value;
+    if (x == "") {
+      alert("Address must be filled out; like, how will we get the tree to you otherwise?");
+      return false;
+    }
+  }
+
+  //function validateForm() {
+  //  var x = document.forms["myForm"]["fname"].value;
+  //  if (x == "") {
+  //    alert("Name must be filled out");
+  //    return false;
+  //  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let selOrange= ['round', 'orange'];
+let selLime= ['green', 'oblong'];
+let selLemon= ['yellow','oblong'];
+let selGrapefruit=['round', 'yellow'];
+
+var monoPrompt = window.prompt("Is the fruit round or oblong?");
+var duoPrompt = window.prompt("Is the fruit orange?"); /*no=0, yes=1 
+var triPrompt = window.prompt("Is the fruit yellow?");/
+var tetraPrompt = window.prompt("Is the fruit green?");
+
+if monoPrompt= (/*selOrange 0){
+
+}
+    else if {
+        //
+    }
+    else if {
+
+    else if {
+    }
+}*/
