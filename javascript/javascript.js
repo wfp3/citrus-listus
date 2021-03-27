35f320d6737b02346b91fd985b499016d2aae1ec
“William <umachattahendo@mail.com>
2021-03-27 14:12:39 -0400

    some test javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,28 @@
+function validate(){
+  var username=document.getElementById ("uname");
+  var password=document.getElementById ("pass");
+
+  if(username.value=="" ||password.value==""){
+    alert("Please fill in your response.");
+  }
+  else{}
+}
 
 
-/*function validateForm() {
+
+
+/*w3 sample:
+function validateForm() {
   var x = document.forms["myForm"]["fname"].value;
   if (x == "") {
     alert("Name must be filled out");
     return false;
   }
-} */
+} 
 
-function validateForm() {
-    var x = firstName.forms["formFreeTree"]["firstname"].value;
-    if (x == "") {
-      alert("First name must be filled out");
-      return false;
-    }
-  } 
 
-  function validateForm() {
+
+ /* function validateForm() {
     var x = document.forms["formFreeTree"]["lastname"].value;
     if (x == "") {
       alert("Last name must be filled out; we need it to mine your data.");
@@ -52,7 +58,9 @@ function validateForm() {
 
 
 
-/*let selOrange= ['round', 'orange'];
+/*Early putzing over dichotomous tree: 1: choose 0 or 1 from arrays; 2: prompt questions based on arrays(0/no, 1/yes) 3: else/if statements to get the answer
+
+let selOrange= ['round', 'orange'];
 let selLime= ['green', 'oblong'];
 let selLemon= ['yellow','oblong'];
 let selGrapefruit=['round', 'yellow'];
