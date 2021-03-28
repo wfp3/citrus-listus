879b24372377d9baa356162a57ba1e079ac27e79
“William <umachattahendo@mail.com>
2021-03-28 14:31:30 -0400

    working javascript form

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,21 +3,18 @@ function validate(){
   var fName= document.getElementById("firstName").value.trim();
   var lName= document.getElementById("lastName").value.trim();
   var stAddy= document.getElementById("streetAddy").value.trim();
-  var staAddy= document.getElementById("stateAddy").value.trim()
+  var staAddy= document.getElementById("stateAddy").value.trim();
 
-    if(fName.value=="");
-    if(lName.value=="");
-    if(stAddy.value=="");
-    if(staAddy.value=="");{
-
+    if (fName=="" || lName=="" || stAddy=="" || staAddy=="") {
       alert("Please fill in the blank field.");
+      
       return false;
     }
       else{
-        true;
+        console.log('hitting the else')
+        return true;
       }
     }
-}
 
 
 
